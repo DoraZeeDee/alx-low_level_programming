b3551c8193ab6c83d83833f2be0280b64878c8a3
DoraZeeDee <doraozioma@gmail.com>
2022-09-14 22:08:53 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case.
+ * @x: contains value to be compared
+ * Result: 1 (success)
+ * Return: 0 else
+ */
+int _islower(int x)
+
+{
+if (x >= 'a' && x <= 'z')
+
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+}
+
