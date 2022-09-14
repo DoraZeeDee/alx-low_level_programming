452c0dc28ac3bc12ad72e363a3b4179f7e484efc
DoraZeeDee <doraozioma@gmail.com>
2022-09-14 16:42:21 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Returni: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x < 'z'; x++)
+	_putchar(x);
+	_putchar('\n');
+}
