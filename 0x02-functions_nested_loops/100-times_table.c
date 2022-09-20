c467a3c9daa7a2e20a03c94a72ac55063dbbcfc3
DoraZeeDee <doraozioma@gmail.com>
2022-09-20 21:09:58 +0000

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,48 +2,48 @@
 
 /**
  * print_times_table - prints times table, starting with 0.
- * @x: the variable that decides what is printed
+ * @n: the variable that decides what is printed
  * Return:void
  */
 
 void print_times_table(int n)
 {
 
-	int a = 0, rep, b;
+int a = 0, rep, b;
 
-	if (n < 0 || n > 15)
-		return;
+if (n < 0 || n > 15)
+return;
 
-	while (a <= n)
-	{
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
+while (a <= n)
+{
+for (b = 0; b <= n; b++)
+{
+rep = a * b;
+if (b == 0)
+_putchar('0' + rep);
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + rep);
 
-			}
-			else if (rep < 100)
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
+}
+else if (rep < 100)
+{
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 10);
+_putchar('0' + rep % 10);
 
-			}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		}
-		_putchar('\n');
-		a++;
+}
+if (b < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+a++;
 
 
-	}
+}
 }
