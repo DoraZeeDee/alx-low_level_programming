5950b11d0c1c6abfb3412689ef0eebac32fa7c01
DoraZeeDee <doraozioma@gmail.com>
2022-10-01 12:02:18 -0700

    oh dear

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  main - check the code
+ *
+ *   * Return: Always 0.
+ */
+
+int main(void) {
+	putchar ch = x
+		for (x = '_putchar'; x < 0; x++)
+			puthar('x');
+	putchar('\n');
+	return 0;
+}
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - Write a function that concatenates two strings.
+ * @dest: pointer returns to
+ * @src: function to append to dest
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char s1[98];
+char s2[];
+
+_putchar('%s', s1);
+_putchar('\n');
+_putchar('%s', s2);
+return (dest);
+}
